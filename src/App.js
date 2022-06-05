bcf4dc0923377a6024d0c02960af8fecd96cebbb
ibktommy <atomquartz@gmail.com>
2022-06-05 17:34:29 +0100

    Condition to check if loading state is true or not

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Loading';
 import Loading from './Loading';
 
 function App() {
   // DATA URL
   const url = 'https://course-api.com/react-tours-project';
+
+  // App Level State
+  const [loading, setLoading] = useState(true)
+
+  // Condition to chech if "loading" state is true or not
+  if (loading) {
+    return (
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
   return (
     <>
       <h2>Tours Web App</h2>
-      <Loading/>
     </>
   )
 }
