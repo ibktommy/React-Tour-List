e24958059ac0446abac2fbe1c6e1f4dae894debc
ibktommy <atomquartz@gmail.com>
2022-06-05 17:29:44 +0100

    Adding Needed Components and data URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import "./App.css";
+import React from 'react';
+import './Loading';
+import Loading from './Loading';
 
 function App() {
-	return (
-		<>
-			<h1 className="text-4xl">Hello</h1>
-			<div className="bg-red-600">A div</div>
-		</>
-	);
+  // DATA URL
+  const url = 'https://course-api.com/react-tours-project';
+  return (
+    <>
+      <h2>Tours Web App</h2>
+      <Loading/>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="text-center font-bold text-5xl mt-[20vh] text-gray-700">Loading...</div>
+  )
+}
+
+export default Loading
